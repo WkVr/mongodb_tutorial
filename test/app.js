c1edb81724dc435c3df600bbcf841384ac694b85
Wynand van Rensburg <wkvanrensburg@gmail.com>
2023-06-01 16:30:46 +0200

    Connect to cluster and list databases

--- test/app.js
+++ test/app.js
@@ -1,4 +1,29 @@
 const { MongoClient } = require('mongodb');
 const uri = require('./atlas_uri');
 
-console.log(uri);
+const client = new MongoClient(uri);
+const dbname = 'tutorial';
+
+const connectToDatabase = async () => {
+    try{
+        await client.connect();
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+const main = async () => {
+    try{
+        await connectToDatabase();
+        const databaseList = await client.db().admin().listDatabases();
+        databaseList.databases.forEach(db => console.log(db.name));
+    }
+    catch(e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main();
